1efb9810201382a3995652db09596785f346fbde
Thành Tri <apple@ThanhTri.local>
2022-08-13 17:51:49 +0700

    sua bai tap

--- ss02/exercise/src/index.ts
+++ ss02/exercise/src/index.ts
@@ -1,6 +1,6 @@
 function fibonacciNumber(n: number): number {
-    if (n == 0 || n == 1) {
-        return 1;
+    if (n <= 1) {
+        return n;
     } else {
         return fibonacciNumber(n - 2) + fibonacciNumber(n - 1);
     }
@@ -8,9 +8,7 @@ function fibonacciNumber(n: number): number {
 
 let sum = 0;
 
-console.log("20 số fibonacci đầu tiên là:");
-
-for (let i = 1; i <= 20; i++) {
+for (let i = 0; i <= 20; i++) {
     console.log(fibonacciNumber(i))
     sum += fibonacciNumber(i);
 }
