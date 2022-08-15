830f32d481b91197297b871705d5aa54ef3e3b58
Thành Tri <apple@ThanhTri.local>
2022-08-15 20:24:42 +0700

    sua bai

--- Case_Study/task2/src/app/app.module.ts
+++ Case_Study/task2/src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { FacilityListComponent } from './facility/facility-list/facility-list.component';
+import { FacilityCreateComponent } from './facility/facility-create/facility-create.component';
+import { FacilityEditComponent } from './facility/facility-edit/facility-edit.component';
+import { ContractListComponent } from './contract/contract-list/contract-list.component';
+import { FacilityDeleteComponent } from './facility/facility-delete/facility-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    CustomerEditComponent,
+    FacilityListComponent,
+    FacilityCreateComponent,
+    FacilityEditComponent,
+    ContractListComponent,
+    FacilityDeleteComponent
   ],
   imports: [
     BrowserModule,
